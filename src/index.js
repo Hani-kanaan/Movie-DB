a158578b9df313ad1cce261864fca9aa0000c98a
Hani-kanaan <hani.kanaan85@yahoo.com>
2024-12-07 02:44:48 +0200

    fixing deployment

--- src/index.js
+++ src/index.js
@@ -11,25 +11,7 @@ root.render(
   </React.StrictMode>
 );
 
-
-require('dotenv').config()
-const registerRouter = require('./routes/register')
-const express = require('express')
-const mongoose = require('mongoose')
-const db = mongoose.connection
-const PORT = process.env.PORT || 5010
-const app = express()
-app.use(express.json())
-mongoose.connect(process.env.DATABASE_URL,
- {
- useNewUrlParser: true,
- useUnifiedTopology: true
- })
-mongoose.set('strictQuery', true)
-db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('Connected to Database'))
-app.listen(PORT, () => console.log(`Server Started on port ${PORT}`))
-
+ 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
