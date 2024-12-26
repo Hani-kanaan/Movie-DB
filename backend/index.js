76967f6532932247e07c614040f91990bcaa1cb8
Hani-kanaan <hani.kanaan85@yahoo.com>
2024-12-26 19:53:49 +0200

    cors fix

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,18 @@
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
-import userRoutes from './routes/userRoutes.js';
-
+import userRoutes from "./routes/userRoutes.js";
+import cors from "cors";
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT;
 const mongoDBURL = process.env.MONGODB_URL;
-
 //for parsing json body:
 app.use(express.json());
-app.use('/api/user', userRoutes);
+app.use("/api/user", userRoutes);
 
-
-mongoose
-  .connect(mongoDBURL)
-  .then(() => {
-    console.log("app connected to DB");
-    app.listen(PORT, () => {
-      console.log(`app listening to port: ${PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+//for cors error
+app.use(cors({
+  origin: "https://movie-db-t8nd.onrender.com",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+}));
